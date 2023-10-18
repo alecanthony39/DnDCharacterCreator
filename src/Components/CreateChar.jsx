a0520fe81e73e18f8438a0c31b2b4264e8449700
alecanthony <alecanthony39@gmail.com>
2023-10-18 08:27:56 -0500

    Bug with point distrubution

--- src/Components/CreateChar.jsx
+++ src/Components/CreateChar.jsx
@@ -3,6 +3,58 @@ import { useState } from "react";
 
 const CreateC = () => {
   const [character, setCharacter] = useState({});
+  const [stats, setStats] = useState({
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10,
+  });
+  const [pointsSpent, setPointsSpent] = useState(0);
+  const [totalPointsLimit, setTotalPointsLimit] = useState(27);
+
+  const handleStatChange = (statName, value) => {
+    const initialPoints = 10;
+    const newStats = {
+      ...stats,
+      [statName]: value,
+    };
+
+    const newPointsSpent = Object.values(newStats).reduce(
+      (total, statValue) => total + Math.max(statValue - initialPoints, 0),
+      0
+    );
+
+    const isValidValue = value >= 8 && value <= 20;
+    const isValidChange = newPointsSpent <= totalPointsLimit;
+
+    console.log("isValidValue", isValidValue);
+    console.log("isValidChange", isValidChange);
+
+    if (isValidValue && isValidChange) {
+      setStats(newStats);
+      setPointsSpent(newPointsSpent);
+      setTotalPointsLimit(27 - newPointsSpent);
+    }
+  };
+
+  const renderStatInputs = () => {
+    return Object.keys(stats).map((statName) => (
+      <div key={statName}>
+        <label>
+          {statName.charAt(0).toUpperCase() + statName.slice(1)}:
+          <input
+            type="number"
+            value={stats[statName]}
+            onChange={(e) =>
+              handleStatChange(statName, parseInt(e.target.value))
+            }
+          />
+        </label>
+      </div>
+    ));
+  };
 
   const handleInputChange = (fieldName, value) => {
     setCharacter((prevCharacter) => ({
@@ -31,10 +83,8 @@ const CreateC = () => {
         <label>
           Character Class:
           <select
-            value={character.characterClass || ""}
-            onChange={(e) =>
-              handleInputChange("characterClass", e.target.value)
-            }
+            value={character.class || ""}
+            onChange={(e) => handleInputChange("class", e.target.value)}
           >
             <option value="">Select Class</option>
             <option value="Wizard">Wizard</option>
@@ -67,8 +117,54 @@ const CreateC = () => {
           </select>
         </label>
         <br />
-        {/* Adding More Soon */}
+        <label>
+          Character Background
+          <select
+            value={character.background || ""}
+            onChange={(e) => handleInputChange("background", e.target.value)}
+          >
+            <option value="">Select Background</option>
+            <option value="Acolyte">Acolyte</option>
+            <option value="Charlatan">Charlatan</option>
+            <option value="Criminal">Criminal</option>
+            <option value="Entertainer">Entertainer</option>
+            <option value="Folk Hero">Folk Hero</option>
+            <option value="Guild Artisan">Guild Artisan</option>
+            <option value="Hermit">Hermit</option>
+            <option value="Noble">Noble</option>
+            <option value="Outlander">Outlander</option>
+            <option value="Sage">Sage</option>
+            <option value="Sailor">Sailor</option>
+            <option value="Soldier">Soldier</option>
+            <option value="Urchin">Urchin</option>
+          </select>
+        </label>
         <br />
+
+        <label>
+          {" "}
+          Alignment
+          <select
+            value={character.alignment}
+            onChange={(e) => handleInputChange("alignment", e.target.value)}
+          >
+            <option value="">Select Alignment</option>
+            <option value="Lawful Good">Lawful Good</option>
+            <option value="Neutral Good">Neutral Good</option>
+            <option value="Chaotic Good">Chaotic Good</option>
+            <option value="Lawful Neutral">Lawful Neutral</option>
+            <option value="True Neutral">True Neutral</option>
+            <option value="Chaotic Neutral">Chaotic Neutral</option>
+            <option value="Lawful Evil">Lawful Evil</option>
+            <option value="Neutral Evil">Neutral Evil</option>
+            <option value="Chaotic Evil">Chaotic Evil</option>
+          </select>
+        </label>
+        <br />
+        <h1>Points Left:{totalPointsLimit}</h1>
+        {renderStatInputs()}
+        <br />
+
         <button type="submit">Submit</button>
       </form>
     </div>
